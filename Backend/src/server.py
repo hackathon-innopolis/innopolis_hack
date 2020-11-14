0d91157655b670fe65c1861d1b86eae637e97434
umutaev <M8858110177@yandex.com>
2020-11-14 18:19:42 +0500

    Added a little bit of bad sh@t.

--- Backend/src/server.py
+++ Backend/src/server.py
@@ -1,7 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 import sqlite3 as sql
 import json
+from io import StringIO
+from PIL import Image
 app = Flask(__name__)
+counter = 0
+
 
 @app.route("/fetch", methods = ['POST'])
 def make_someones_dream_come_true():
@@ -50,10 +54,20 @@ def get_balance():
     name = request.form['name']
     return str(bal(name))
 
+@app.route("/buy", methods=['POST'])
+def buy_a_little_bit():
+    with sql.connect("./database/main.db") as conn:
+        name = request.form['name']
+        count = request.form['count']
+        c = conn.cursor()
+        c.execute("UPDATE balance SET bal = bal + {} WHERE name \"{}\"".format(count, name))
+        return "OK"
+
 @app.route("/map", methods = ['POST'])
 def get_map():
     xcoord = request.form['x']
     ycoord = request.form['y']
+    return make_a_map([float(xcoord), float(ycoord)])
 
 def bal(name):
     with sql.connect("./database/main.db") as conn:
@@ -67,6 +81,25 @@ def bal(name):
         c.execute("INSERT INTO balance (name, bal) VALUES ('{}', {})".format(name, 1))
         conn.commit()
         return 1
+
+def convert(coordniate):
+    coord = [0, 0]
+    coord[0] = int((coordniate[1] - 60.5) / 0.2 * 8506)
+    coord[1] = int((coordniate[0] - 56.8) / 0.1 * 9322)
+    return coord
+
+def make_a_map(coordinate):
+    global counter
+    coordinate = convert(coordinate)
+    print(coordinate)
+    img = Image.open('../img/full_map.png')
+    k = 350
+    area = (coordinate[0] - k, coordinate[1] - k, coordinate[0] + k, coordinate[1] + k)
+    new_img = img.crop(area)
+    new_img.save("{}.png".format(counter))
+    counter += 1
+    return send_file('{}.png'.format(counter - 1), mimetype='image/png')
+
 
 if __name__ == "__main__":
     app.run()

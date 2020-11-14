f636da1c2acd351ad8f96c42d1e53df59da0ccdd
PetrovDmitryS <PetrovDmitryS@users.noreply.github.com>
2020-11-14 17:48:26 +0500

    Added method, that cut map and returns image.

--- Maps/cutter.py
+++ Maps/cutter.py
@@ -2,12 +2,20 @@ from PIL import Image
 # from smth import coordinate
 
 
+def convert(coordniate):
+    coordinate[0] = (coordniate[0] - 56.8) / 0.1 * 8506
+    coordinate[1] = (coordniate[1] - 60.5) / 0.2 * 9322
+    return coordniate
+
+
 def main(coordinate):
+    coordinate = convert(coordinate)
     img = Image.open('full_map.png')
-    area = (coordinate[0] - 350, coordinate[1] - 350, coordinate[0] + 350, coordinate[1] + 350)
+    k = 350
+    area = (coordinate[0] - k, coordinate[1] - k, coordinate[0] + k, coordinate[1] + k)
     new_img = img.crop(area)
     return new_img
 
 
-coordinate = (4500, 5350)
+coordinate = [4500, 5350]
 main(coordinate).show()

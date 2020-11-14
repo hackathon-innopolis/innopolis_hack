419d44454dd51c5529890b187a8818be988c533c
Fodro <feodor.tomilov@yandex.ru>
2020-11-14 16:22:38 +0500

    Location enabled in another activity

--- Android/app/src/main/java/com/voronin/jinn/SubmitActivity.java
+++ Android/app/src/main/java/com/voronin/jinn/SubmitActivity.java
@@ -1,21 +1,32 @@
 package com.voronin.jinn;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class SubmitActivity extends AppCompatActivity {
 
     private EditText wishToSend;
     private DBHelper dbHelper;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,25 @@ public class SubmitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_submit);
         wishToSend = findViewById(R.id.wish);
         dbHelper = new DBHelper(this);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void getLoc() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            shouldShowRequestPermissionRationale("ACCESS_COARSE_LOCATION");
+            shouldShowRequestPermissionRationale("ACCESS_FINE_LOCATION");
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            //TODO map
+                        }
+                    }
+                });
     }
 
     public void onClick(View view){
